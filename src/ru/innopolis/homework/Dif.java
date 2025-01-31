8a6fb610f9d4aa655c64d5bfb4b41b6c93c88427
albertalexandrov <albert-ugatu@list.ru>
2025-01-31 12:25:02 +0500

    тест

---
+++ src/ru/innopolis/homework/Dif.java
@@ -0,0 +1,16 @@
+package ru.innopolis.homework;
+import java.lang.reflect.Modifier;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Dif {
+    public static void main(String[] args) {
+        String s = "string";
+        System.out.println(Collectors.toList().getClass());
+
+    }
+}
