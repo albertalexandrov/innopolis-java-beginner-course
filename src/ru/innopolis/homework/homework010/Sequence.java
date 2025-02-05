595dbc010f7cf35aacc274db561efe6f7a76a2ff
albertalexandrov <albert-ugatu@list.ru>
2025-02-05 12:58:07 +0500

    ДЗ10. Выполнены проверки

--- src/ru/innopolis/homework/homework010/Sequence.java
+++ src/ru/innopolis/homework/homework010/Sequence.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 
 public class Sequence {
     public static int[] filter(int[] array, ByCondition condition) {
+        // todo:
+        //  если я правильно понял, то для IntStream предикат должен быть либо IntPredicate,
+        //  либо кастомный, но представляющий метод с аргументом int?
+        //  без ::isOk IDE указывала ошибку
         return Arrays.stream(array).filter(condition::isOk).toArray();
     }
 }
