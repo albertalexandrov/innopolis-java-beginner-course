633fe0042f92711676394bbb5caf3dd5196bb657
albertalexandrov <albert-ugatu@list.ru>
2025-02-08 18:41:58 +0500

    ДЗ11. Выполнены проверки

---
+++ src/ru/innopolis/homework/homework11/App.java
@@ -0,0 +1,59 @@
+package ru.innopolis.homework.homework11;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        final var INPUT_FILE_PATH = "src/ru/innopolis/homework/homework11/input.txt";
+        final var OUTPUT_FILE_PATH = "src/ru/innopolis/homework/homework11/output.txt";
+        final var CAR_ROW_FORMAT = "%-8s\t%-8s\t%-8s\t%-8s\t%-8s\n";
+
+        try (
+                var reader = new BufferedReader(new FileReader(INPUT_FILE_PATH));
+                var writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH));
+        ) {
+            writer.write("Автомобили в базе:\n");
+            writer.write(String.format(CAR_ROW_FORMAT, "Number", "Model", "Color", "Mileage", "Cost"));
+            reader.readLine();  // пропускаем первую строку, которая несет в себе заголовки;
+            String line;
+            var cars = new ArrayList<Car>();
+            while ((line = reader.readLine()) != null) {
+                var tokens = line.split("\\|");
+                var car = createCar(tokens);
+                cars.add(car);
+                System.out.println("Создан автомобиль: " + car);
+                writer.write(String.format(CAR_ROW_FORMAT, car.getNumber(), car.getModel(), car.getColor(), car.getMileage(), car.getCost()));
+            }
+
+            writer.write("\n");
+
+            // вывести номера всех автомобилей, имеющих заданный в переменной цвет colorToFind или нулевой пробег mileageToFind
+
+            var colorToFind = "Black";
+            var mileageToFind = 0;
+            var numbers = cars.stream()
+                    .filter(car -> car.getColor().equals(colorToFind) || car.getMileage().equals(mileageToFind))
+                    .map(Car::getNumber)
+                    .collect(Collectors.joining(" "));
+            writer.write(String.format("Номера автомобилей по цвету или пробегу: %s\n", numbers));
+
+            // вывести количество уникальных моделей в ценовом диапазоне от n до m тыс.
+
+            var n = 700_000;
+            var m = 800_000;
+            var count = cars.stream().filter(car -> n < car.getCost() && car.getCost() < m).count();
+            writer.write(String.format("Уникальные автомобили: %d шт.", count));
+        }
+    }
+
+    private static Car createCar(String[] tokens) {
+        var number = tokens[0];
+        var model = tokens[1];
+        var color = tokens[2];
+        var mileage = Integer.parseInt(tokens[3]);
+        var cost = Integer.parseInt(tokens[4]);
+        return new Car(number, model, color, mileage, cost);
+    }
+}
--- src/ru/innopolis/homework/homework11/Car.java
+++ src/ru/innopolis/homework/homework11/Car.java
@@ -1,20 +1,18 @@
 package ru.innopolis.homework.homework11;
 
-import java.util.Objects;
-
 public class Car {
     private String number;
     private String model;
     private String color;
-    private int mileage;
-    private int price;
+    private Integer mileage;
+    private Integer cost;
 
-    public Car(String number, String model, String color, int mileage, int price) {
+    public Car(String number, String model, String color, Integer mileage, int cost) {
         this.number = number;
         this.model = model;
         this.color = color;
         this.mileage = mileage;
-        this.price = price;
+        this.cost = cost;
     }
 
     public Car() {
@@ -44,20 +42,20 @@ public class Car {
         this.color = color;
     }
 
-    public int getMileage() {
+    public Integer getMileage() {
         return mileage;
     }
 
-    public void setMileage(int mileage) {
+    public void setMileage(Integer mileage) {
         this.mileage = mileage;
     }
 
-    public int getPrice() {
-        return price;
+    public Integer getCost() {
+        return cost;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
+    public void setCost(Integer cost) {
+        this.cost = cost;
     }
 
     @Override
@@ -67,18 +65,7 @@ public class Car {
                 ", model='" + model + '\'' +
                 ", color='" + color + '\'' +
                 ", mileage=" + mileage +
-                ", price=" + price +
+                ", cost=" + cost +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Car car)) return false;
-        return mileage == car.getMileage() && price == car.getPrice() && Objects.equals(number, car.getNumber()) && Objects.equals(model, car.getModel()) && Objects.equals(color, car.getColor());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(number, model, color, price, mileage);
     }
 }
