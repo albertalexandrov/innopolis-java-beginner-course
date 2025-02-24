8346eb5cd033b1792b942919bd12bf9fba6955ea
albertalexandrov <albert-ugatu@list.ru>
2025-02-24 14:24:15 +0300

    В класс User добавлены поля, геттеры и сеттеры

--- src/ru/innopolis/attestations/attestation01/models/User.java
+++ src/ru/innopolis/attestations/attestation01/models/User.java
@@ -1,7 +1,95 @@
 package ru.innopolis.attestations.attestation01.models;
 
+import java.time.LocalDateTime;
+
 public class User {
     private String id;
+    private LocalDateTime createdAt;
+    private String login;
+    private String password;
+    private String confirmPassword;
+    private String lastName;
+    private String firstName;
+    private String middleName;
+    private int age;
+    private boolean isWorker;
+
+    public User(String line) {
+        String[] tokens = line.split("\\|");
+        this.id = tokens[0];
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isWorker() {
+        return isWorker;
+    }
+
+    public void setWorker(boolean worker) {
+        isWorker = worker;
+    }
 
     public String getId() {
         return id;
@@ -10,4 +98,7 @@ public class User {
     public void setId(String id) {
         this.id = id;
     }
+
+
+
 }
