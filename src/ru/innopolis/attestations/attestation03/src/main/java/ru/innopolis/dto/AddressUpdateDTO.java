ef1e521e3ef152c43e8809f0f2ce768377692f83
albertalexandrov <albert-ugatu@list.ru>
2025-04-26 12:21:25 +0300

    Добавлены описания и примеры для ДТО AddressCreateDTO и AddressUpdateDTO

--- src/ru/innopolis/attestations/attestation03/src/main/java/ru/innopolis/dto/AddressUpdateDTO.java
+++ src/ru/innopolis/attestations/attestation03/src/main/java/ru/innopolis/dto/AddressUpdateDTO.java
@@ -1,5 +1,6 @@
 package ru.innopolis.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,12 +8,24 @@ import lombok.Setter;
 @Setter
 public class AddressUpdateDTO {
 
+    @Schema(description = "Населенный пункт", example = "г. Казань")
     public String locality;
+
+    @Schema(description = "Улица", example = "ул. Павлюхина")
     public String street;
+
+    @Schema(description = "Дом", example = "1")
     public String house;
+
+    @Schema(description = "Подъезд", example = "2")
     public String porch;
+
+    @Schema(description = "Этаж", example = "3")
     public String floor;
+
+    @Schema(description = "Квартира", example = "4")
     public String apartment;
+
     // тут нет userId
 
 }
